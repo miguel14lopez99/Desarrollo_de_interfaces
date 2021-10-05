e86c969467cb2a832998745b0512f9a6f9b67958
Miguel <miguel14lopez99@gmail.com>
2021-10-05 13:23:22 +0200

    He hecho hasta el ejercicio 18 (falta el 17)

---
+++ Ejercicios de C#/Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            double res = TrigonometricSeries.sin(num);
+            Console.WriteLine("The sin of " + num + " is: " + res);
+
+            res = TrigonometricSeries.cos(num);
+            Console.WriteLine("The cos of " + num + " is: " + res);
+
+        }
+    }
+}
---
+++ Ejercicios de C#/Ejercicio18/Ejercicio18/TrigonometricSeries.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio18
+{
+    class TrigonometricSeries
+    {
+
+        public static double sin(double x)
+        {
+            double res = 0;
+            int opt = 1;
+
+            for (int i = 1; i < 10; i += 2)
+            {
+
+                switch (opt)
+                {
+                    case 1:
+                        res += (Math.Pow(x, i) / factorial(i));
+                        opt = 2;
+                        break;
+
+                    case 2:
+                        res -= (Math.Pow(x, i) / factorial(i));
+                        opt = 2;
+                        break;
+                }
+
+            }
+
+            return res;
+        }
+
+        public static double cos(double x)
+        {
+            double res = 0;
+            int opt = 1;
+
+            for (int i = 0; i < 10; i += 2)
+            {
+
+                switch (opt)
+                {
+                    case 1:
+                        res += (Math.Pow(x, i) / factorial(i));
+                        opt = 2;
+                        break;
+
+                    case 2:
+                        res -= (Math.Pow(x, i) / factorial(i));
+                        opt = 2;
+                        break;
+                }
+
+            }
+
+            return res;
+        }
+
+        private static double factorial(double num)
+        {
+
+            //Falta el factorial
+
+        }
+
+    }
+}
