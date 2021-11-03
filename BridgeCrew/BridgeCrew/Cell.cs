a3c12ff3d80e042eb9cad6c3af78cd7b5eb62a03
Miguel <miguel14lopez99@gmail.com>
2021-11-03 14:19:13 +0100

    creado el proyecto bridge crew, cambiado el uml

---
+++ BridgeCrew/BridgeCrew/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeCrew
+{
+    class Cell
+    {
+
+        private int idSquare;
+        private Boolean damaged;
+
+
+
+    }
+}
