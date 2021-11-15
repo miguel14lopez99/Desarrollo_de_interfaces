cf959fea114febbea5bfd936afebf604d5c6c5d2
Miguel <miguel14lopez99@gmail.com>
2021-11-15 08:48:12 +0100

    guardado

---
+++ ExampleBD_MVC_WPF/ExampleBD_MVC_WPF/NewUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ExampleBD_MVC_WPF.Domain;
+
+namespace ExampleBD_MVC_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para NewUser.xaml
+    /// </summary>
+    public partial class NewUser : Window
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            User aux = new User();
+
+            aux.name = txtName.Text;
+            aux.password = txtPass.Password;//cambiar
+
+        }
+    }
+}
